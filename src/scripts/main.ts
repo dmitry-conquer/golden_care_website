8788f020bb5c0924a8a0aa6dcb2420e7447e44aa
Dmitry Conquer <dmitry.conquer@gmail.com>
2025-05-26 18:48:20 +0300

    feat: implement accordion component and integrate into main script; add serve areas and FAQ sections to front page

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -2,12 +2,15 @@ import Sliders from "./components/sliders";
 import Scroll from "./scroll";
 import BackTopButton from "./components/back-top-button";
 import GLightbox from "glightbox";
+import AccordionCollection from "./components/accordion";
 import "../styles/main.scss";
 import "glightbox/dist/css/glightbox.min.css";
 
 document.addEventListener("DOMContentLoaded", () => {
   new Sliders();
   new BackTopButton(900, "home");
+  new AccordionCollection();
+
   GLightbox({
     selector: ".video-lightbox",
   });
