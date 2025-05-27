5485f763413cfa7aeb404b166ad79db07c97f68a
Dmitry Conquer <dmitry.conquer@gmail.com>
2025-05-27 11:34:16 +0300

    feat: enhance header component with accessibility improvements; add mobile logo and background image; update styles for active state

--- src/scripts/components/header.ts
+++ src/scripts/components/header.ts
@@ -5,6 +5,10 @@ export class Header {
     triggerButton: "[data-js-header-trigger-button]",
   };
 
+  private readonly attributes: Record<string, string> = {
+    ariaExpanded: "aria-expanded",
+  };
+
   private readonly stateClasses: Record<string, string> = {
     isActive: "is-active",
     isLock: "is-lock",
@@ -13,11 +17,16 @@ export class Header {
   private rootElement: HTMLElement | null;
   private overlayElement: HTMLElement | null;
   private triggerButtonElement: HTMLElement | null;
+  private toggleElements: HTMLElement[];
 
   constructor() {
     this.rootElement = document.querySelector(this.selectors.root);
     this.overlayElement = this.rootElement?.querySelector(this.selectors.overlay) || null;
     this.triggerButtonElement = this.rootElement?.querySelector(this.selectors.triggerButton) || null;
+
+    this.toggleElements = [this.rootElement, this.overlayElement, this.triggerButtonElement].filter(
+      Boolean
+    ) as HTMLElement[];
 
     this.init();
   }
@@ -34,16 +43,23 @@ export class Header {
   private onDocumentClick = (e: MouseEvent): void => {
     const target = e.target as HTMLElement;
     if (target.closest(this.selectors.triggerButton) || target.closest(this.selectors.overlay)) return;
-    this.triggerButtonElement?.classList.remove(this.stateClasses.isActive);
-    this.overlayElement?.classList.remove(this.stateClasses.isActive);
-    document.documentElement.classList.remove(this.stateClasses.isLock);
+    this.setActive(false);
   };
 
   private onTriggerButtonClick = (): void => {
-    this.triggerButtonElement?.classList.toggle(this.stateClasses.isActive);
-    this.overlayElement?.classList.toggle(this.stateClasses.isActive);
-    document.documentElement.classList.toggle(this.stateClasses.isLock);
+    const isActive = this.rootElement?.classList.contains(this.stateClasses.isActive);
+    this.setActive(!isActive);
   };
+
+  private setActive(state: boolean): void {
+    this.toggleElements.forEach(el => el.classList.toggle(this.stateClasses.isActive, state));
+    document.documentElement.classList.toggle(this.stateClasses.isLock, state);
+    this.setAttributes(state);
+  }
+
+  private setAttributes(state: boolean): void {
+    this.triggerButtonElement?.setAttribute(this.attributes.ariaExpanded, String(state));
+  }
 
   bindEvents(): void {
     this.triggerButtonElement?.addEventListener("click", this.onTriggerButtonClick);
