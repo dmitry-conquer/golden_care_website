f6613465d6aa32317ce9eca416f2d55a28f8dd5b
Dmitry Conquer <dmitry.conquer@gmail.com>
2025-09-02 14:14:53 +0300

    Додано підменю для розділу "Наші послуги" в HTML, оновлено логіку обробки подій у TypeScript для підтримки нових функцій меню. Оновлено SCSS стилі для покращення адаптивності та візуальних ефектів меню.

--- src/scripts/components/header.ts
+++ src/scripts/components/header.ts
@@ -19,13 +19,14 @@ export class Header {
   private triggerButtonElement: HTMLElement | null;
   private toggleElements: HTMLElement[];
   private scroll: any;
+  private itemHasSubmenuElements: HTMLElement[];
 
   constructor(scroll: any) {
     this.scroll = scroll;
     this.rootElement = document.querySelector(this.selectors.root);
     this.overlayElement = this.rootElement?.querySelector(this.selectors.overlay) || null;
     this.triggerButtonElement = this.rootElement?.querySelector(this.selectors.triggerButton) || null;
-
+    this.itemHasSubmenuElements = Array.from(this.rootElement?.querySelectorAll(".menu-item-has-children") || []);
     this.toggleElements = [this.rootElement, this.overlayElement, this.triggerButtonElement].filter(
       Boolean
     ) as HTMLElement[];
@@ -41,12 +42,6 @@ export class Header {
     if (!this.isReady()) return;
     this.bindEvents();
   }
-
-  private onDocumentClick = (e: MouseEvent): void => {
-    const target = e.target as HTMLElement;
-    if (target.closest(this.selectors.triggerButton) || target.closest(this.selectors.overlay)) return;
-    this.setActive(false);
-  };
 
   private onTriggerButtonClick = (): void => {
     const isActive = this.rootElement?.classList.contains(this.stateClasses.isActive);
@@ -67,9 +62,63 @@ export class Header {
     this.triggerButtonElement?.setAttribute(this.attributes.ariaExpanded, String(state));
   }
 
+  get isTouchDevice() {
+    return navigator.maxTouchPoints > 0 || window.matchMedia?.("(pointer: coarse)")?.matches;
+  }
+
+  private toggleSubmenu(currentIndex: number): void {
+    this.itemHasSubmenuElements.forEach((menuItem, index) => {
+      const subMenu = menuItem.querySelector("ul") as HTMLElement;
+      if (!subMenu) return;
+      const active = menuItem.classList.contains("is-active");
+
+      if (currentIndex === index) {
+        menuItem?.classList.toggle("is-active");
+        subMenu.style.maxHeight = active ? "" : `${subMenu.scrollHeight}px`;
+      } else {
+        menuItem.classList.remove("is-active");
+        subMenu.style.maxHeight = "";
+      }
+    });
+  }
+
+  private handleInteraction(index: number, e: MouseEvent) {
+    const menuItem = this.itemHasSubmenuElements[index];
+    const isActive = menuItem.classList.contains("is-active");
+
+    if (!isActive) {
+      e.preventDefault();
+      this.toggleSubmenu(index);
+    }
+  }
+
+  private onDocumentClick = (e: MouseEvent): void => {
+    const target = e.target as HTMLElement;
+    if (
+      target.closest(this.selectors.triggerButton) ||
+      target.closest(this.selectors.overlay) ||
+      target.closest(".menu-item-has-children")
+    )
+      return;
+    this.itemHasSubmenuElements.forEach(menuItem => {
+      const subMenu = menuItem.querySelector("ul") as HTMLElement;
+      if (!subMenu) return;
+      menuItem?.classList.remove("is-active");
+      subMenu.style.maxHeight = "";
+    });
+  };
+
   bindEvents(): void {
     this.triggerButtonElement?.addEventListener("click", this.onTriggerButtonClick);
     document.documentElement?.addEventListener("click", this.onDocumentClick);
+    this.itemHasSubmenuElements.forEach((item, index) => {
+      (item.querySelector(":scope > a") as HTMLElement | null)?.addEventListener("click", (event: MouseEvent) => {
+        if (this.isTouchDevice) {
+          console.log(item);
+          this.handleInteraction(index, event);
+        }
+      });
+    });
   }
 }
 
